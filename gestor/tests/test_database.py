6daa7573742d284fa2ab7692cc61fd710393be89
Alba-spz <albasanchezpozo@gmail.com>
2025-05-02 21:49:58 +0200

    he creado el archivo test_database.py para las pruebas unitarias y también el archivo menu.py dentro de la carpeta 'gestor'

---
+++ gestor/tests/test_database.py
@@ -0,0 +1,39 @@
+import copy
+import unittest
+import database as db
+
+class TestDatabase(unittest.TestCase):
+
+    def setUp(self):
+        db.Clientes.lista = [
+            db.Cliente('18L', 'Hugo', 'González'),
+            db.Cliente('23H', 'Víctor', 'Laso'),
+            db.Cliente('44F', 'Julia', 'Pozo')
+        ]
+
+    def test_buscar_cliente(self):
+        cliente_existente = db.Clientes.buscar('18L')
+        cliente_no_existente = db.Clientes.buscar('77S')
+        self.assertIsNotNone(cliente_existente)
+        self.assertIsNone(cliente_no_existente)
+
+    def test_crear_cliente(self):
+        nuevo_cliente = db.Clientes.crear('60B', 'Manolo', 'Gallardo')
+        self.assertEqual(len(db.Clientes.lista), 4)
+        self.assertEqual(nuevo_cliente.dni, '60B')
+        self.assertEqual(nuevo_cliente.nombre, 'Manolo')
+        self.assertEqual(nuevo_cliente.apellido, 'Gallardo')
+
+    def test_modificar_cliente(self):
+        cliente_a_modificar = copy.copy(db.Clientes.buscar('18L'))
+        cliente_modificado = db.Clientes.modificar('18L', 'Luis', 'Díaz')
+        self.assertEqual(cliente_a_modificar.nombre, 'Hugo')
+        self.assertEqual(cliente_modificado.nombre, 'Luis')
+
+    def test_borrar_cliente(self):
+        cliente_borrado = db.Clientes.borrar('23H')
+        cliente_rebuscado = db.Clientes.buscar('23H')
+        self.assertNotEqual(cliente_borrado, cliente_rebuscado)
+
+if __name__ == '__main__':
+    unittest.main()
