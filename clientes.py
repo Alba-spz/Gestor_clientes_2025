57870abf35e7e2af37e5abef843c2783364f7114
Alba-spz <albasanchezpozo@gmail.com>
2025-05-03 02:57:42 +0200

    he modificado el nombre de un archivo csv para que no me diese errores, he probado que el código escrito hasta el momento se ejecute correctamente y he eliminado archivos que tenía creados de hace tiempo.

--- clientes.py
+++
@@ -1,9 +0,0 @@
-class Cliente:
-    def __init__(self, dni, nombre, apellidos):
-        self.dni = dni
-        self.nombre = nombre
-        self.apellidos = apellidos
-
-    def __repr__(self):
-        return f"Cliente({self.dni}, {self.nombre}, {self.apellidos})"
-    
